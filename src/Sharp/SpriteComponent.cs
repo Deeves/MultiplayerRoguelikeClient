d337bb19df4294506ef7ba56e7a3f2ce472eff5e
Deeves <hipster.terminator@gmail.com>
2024-09-15 21:00:29 -0700

    Update 'Door' to 'door' to keep consistency and began work on EntityData

--- src/Sharp/SpriteComponent.cs
+++ src/Sharp/SpriteComponent.cs
@@ -6,7 +6,19 @@ public partial class SpriteComponent : Sprite2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		SelfModulate = SpiritData.myColor;
+		
+		if (SpiritData.myColor != null)
+		{
+			Modulate = SpiritData.myColor;
+		}
+		if (EntityData.myColor != null)
+		{
+			Modulate = EntityData.myColor;
+		}
+		else
+		{
+			GD.PrintErr("No Data Found");
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
